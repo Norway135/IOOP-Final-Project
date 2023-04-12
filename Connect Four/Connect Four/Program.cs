a978ce734b91d752a3d0829b0a3159186769d3ac
Norway135 <cheeseus107@gmail.com>
2023-04-11 18:28:02 -0600

    created board class

--- Connect Four/Connect Four/Program.cs
+++ Connect Four/Connect Four/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Connect_Four
 {
+    
+    class Board 
+    {
+
+        private const int row = 6;
+
+        private const int col = 7;
+
+    }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
             
+
+
         }
     }
 }
